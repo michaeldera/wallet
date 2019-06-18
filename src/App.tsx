4fdf7a33204ae1e95c3ffede3c606bc040c1ea33
Michael Dera <michaeldera@live.com>
2019-06-18 20:18:27 +0200

    added Beneficiaries

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import Home from './pages/home/Home';
+import Beneficiaries from './pages/beneficiaries/Beneficiaries';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter>    
+      <Link to="/beneficiaries">Beneficiaries</Link>
       <Route path="/" exact component={Home}/>
+      <Route path="/beneficiaries/" component={Beneficiaries}/>
     </BrowserRouter>
   );
 }
